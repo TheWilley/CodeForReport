c066960a4ee9ace3fc4d74e7fe122864a9a364f3
thewilley <thewilley25@gmail.com>
2021-12-04 14:42:03 +0100

    Added settings and bug fixes. Font has been changed.

--- script.js
+++ script.js
@@ -1,58 +1,73 @@
 function copy() {
     // Define variables
     let textarea = document.getElementById("input");
-    let spaces = []; 
+    let spaces = [];
     let indent = 0;
     let newtext = "";
 
-    // Get amount of rows
-    var rows = $('#input').val().split(/\r?\n/);
+    if (document.getElementById("no_indent").checked) {
+        // Get amount of rows
+        var rows = $('#input').val().split(/\r?\n/);
 
-    // Go trough all rows and count spaces before first character
-    for (let i = 0; i < rows.length; i++) {
-        spaces[i] = rows[i].search(/\S|$/);
-    }   
-    
-    // Get lowest amount of spacces in array
-    let min = Math.min.apply(Math, spaces);
+        // Go trough all rows and count spaces before first character
+        for (let i = 0; i < rows.length; i++) {
 
-    // Go trough array and set the amount of characters to remove in each line
-    for(let j = 0; j < spaces.length; j++)
-    {
-        // Do not remove if the indent is already at its lowest point
-        if(min != 0)
-        {
-            // Get array position
-            if(spaces[j] == min)
-            {
-                indent = spaces[j];
+            // Empty lines are ignored
+            if (/\S/.test(rows[i])) {
+                spaces[i] = rows[i].search(/\S|$/);
             }
         }
-    }
 
-    // Go trough all rows and remove indent
-    for (let i = 0; i < rows.length; i++) {
-        rows[i] = rows[i].substring(indent);
+        // Get lowest amount of spaces in array
+        let min = Math.min.apply(Math, spaces);
 
-        // Empty lines are ignored
-        if (/\S/.test(rows[i])) {
-            newtext += rows[i] + "\n";
+        // Go trough array and set the amount of characters to remove in each line
+        for (let j = 0; j < spaces.length; j++) {
+            // Do not remove if the indent is already at its lowest point
+            if (min != 0) {
+                // Get array position
+                if (spaces[j] == min) {
+                    indent = spaces[j];
+                }
+            }
         }
-    }   
 
-    // Set value  on textbox
-    textarea.value = newtext;
+        // Go trough all rows and remove indent
+        for (let i = 0; i < rows.length; i++) {
+            rows[i] = rows[i].substring(indent);
+
+            // Empty lines are ignored
+            if (/\S/.test(rows[i])) {
+                newtext += rows[i] + "\n";
+            }
+        }
+
+        // Set value  on textbox
+        textarea.value = newtext;
+    }
 
     // Select the textarea and copy it's content
     textarea.select();
     document.execCommand("copy");
 
-    // Fade in and out copy alert
+    // Fade in and out copy 
     $("#note").fadeIn(50)
-    $("#note").stop().animate({opacity:'100'}).html('Copied!').fadeOut(820);
+    $("#note").stop().animate({
+        opacity: '100'
+    }).html('Copied!').fadeOut(820);
 }
 
+// Adjust area to size of text
 function textAreaAdjust(element) {
-   element.style.height = "1px";
-   element.style.height = (25+element.scrollHeight)+"px";
+    element.style.height = "1px";
+    element.style.height = (25 + element.scrollHeight) + "px";
+}
+
+// Copy text when pasted
+function auto_copy_text(){
+    if(document.getElementById("auto_copy_text").checked)
+    {
+        // Delay as doing it directly will run function before text is actually processed
+        setTimeout(() => { copy();}, 20);
+    }
 }
